45625813c48b3e239b73fa5e9825e66af4a0d6c5
Henrique Braga <henriquebraga@MacBook-Pro-de-Henrique.local>
2020-05-01 17:54:58 -0300

    Change client fixture to use authentication to make requests for tests

--- drink_partners/conftest.py
+++ drink_partners/conftest.py
@@ -6,6 +6,7 @@ from simple_settings.utils import settings_stub
 from yarl import URL
 
 from drink_partners import app as _app
+from drink_partners.contrib.mongo.client import MongoClient
 
 
 @pytest.fixture(scope='session')
@@ -13,6 +14,22 @@ def loop():
     loop = asyncio.get_event_loop()
     yield loop
     loop.close()
+
+
+@pytest.fixture()
+def mongo_database():
+    return MongoClient()
+
+
+@pytest.fixture(autouse=True)
+async def clean_database(mongo_database):
+    collections_name = await mongo_database.db.list_collection_names()
+
+    [
+        mongo_database.db.drop_collection(c)
+        for c in collections_name
+        if not c.startswith('system')
+    ]
 
 
 @pytest.fixture
@@ -43,8 +60,8 @@ def settings_with_applications(token):
 
 
 @pytest.fixture(autouse=True)
-async def client(aiohttp_client, app):
-    return await aiohttp_client(app)
+async def client(aiohttp_client, token, settings_with_applications, app):
+    return await aiohttp_client(app, headers={'Authorization': token})
 
 
 @pytest.fixture
