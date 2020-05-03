2255623709c1b532ba54ab155324def69aad238b
Henrique Braga <henriquebraga@MacBook-Pro-de-Henrique.local>
2020-05-02 21:18:04 -0300

    Implement save on PartnerCreateView

--- drink_partners/partners/views.py
+++ drink_partners/partners/views.py
@@ -4,7 +4,12 @@ import logging
 from aiohttp import web
 
 from drink_partners.backends.pools.partners import PartnersBackendPool
-from drink_partners.contrib.exceptions import BadRequest, NotFound
+from drink_partners.contrib.exceptions import (
+    BadRequest,
+    Conflict,
+    NotFound,
+    PartnerAlreadyExists
+)
 from drink_partners.contrib.response import JSONResponse
 
 logger = logging.getLogger(__name__)
@@ -40,9 +45,25 @@ class PartnerGetView(web.View):
 
 
 class PartnerCreateView(web.View):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.datasource = PartnersBackendPool.get_default()
 
     async def post(self):
+        partners = await self._validate_data()
+
+        try:
+            await self._save_partners(partners)
+        except PartnerAlreadyExists as error:
+            logger.warning(str(error))
+            raise Conflict(
+                error_message=str(error)
+            )
+        return JSONResponse(status=201, data=json.dumps(partners))
+
+    async def _validate_data(self):
         data = {}
+
         try:
             data = await self.request.json()
         except Exception as e:
@@ -50,4 +71,8 @@ class PartnerCreateView(web.View):
                 error_message=f'Invalid payload:{data} error:{e}'
             )
 
-        return web.json_response(status=201, data=data)
+        return data.get('pdvs') or []
+
+    async def _save_partners(self, partners):
+        for partner in partners:
+            await self.datasource.save(partner)
