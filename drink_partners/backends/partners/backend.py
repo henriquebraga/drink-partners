f779f097e9c81e139086a8ac933508045039bb0e
Henrique Braga <henriquebraga@MacBook-Pro-de-Henrique.local>
2020-05-03 17:03:37 -0300

    Create method abstract in PartnersBackend to search partners by lat long

--- drink_partners/backends/partners/backend.py
+++ drink_partners/backends/partners/backend.py
@@ -14,3 +14,7 @@ class PartnersBackend(abc.ABC):
     @abc.abstractmethod
     async def save(self, payload):  # pragma: no cover
         pass
+
+    @abc.abstractmethod
+    async def search_nearest_by_lng_lat(self, coordinate):  # pragma: no cover
+        pass
